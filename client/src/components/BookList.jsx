8b4cae1950896960f2ab5bc6fa2c6b1633f3843a
Дмитро Худіяш <khudiiash@MacBook-Pro-Dmitro.local>
2020-09-18 20:38:03 +0300

    init

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -17,7 +17,7 @@ function BookList() {
     if (!books.length || (current && !books.some(b => b.title === current.title))) {
         axios.get(proxy + '/books')
            .then(res => {
-               console.log(res.data)
+               console.log(res.data, books)
               if (res.data && !books.length) dispatch(setBooks(books = res.data.filter(book => book.userID === user._id)))
             })
         
