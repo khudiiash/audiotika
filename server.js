39b72d8e67c3a5b413c2d7accaa576fbba4b36c2
Дмитро Худіяш <khudiiash@MacBook-Pro-Dmitro.local>
2020-09-21 22:30:32 +0300

    init

--- server.js
+++ server.js
@@ -117,7 +117,6 @@ io.on('connection', function (socket) {
                                                         audio = audioPath
                                                         return
                                                     }
-
                                                 })
 
                                         });
