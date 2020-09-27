914a15d4c1f32b8c47392db215640e8691eb46bc
Дмитро Худіяш <khudiiash@MacBook-Pro-Dmitro.local>
2020-09-27 17:39:02 +0300

    init

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -67,6 +67,7 @@ function Book({ book }) {
     function playBook() {
         //gsap.to(bookRef.current, .5, {scale: 1.05, repeat: 1, yoyo: true})
         setLoading(true)
+        dispatch(setLoading(true))
         let socket = io(proxy);
         axios.post(proxy + '/user/update-current', {userID: user._id, currentBookID: book._id})
         console.log('Book: downloading current chapter: ', book.chapter)
@@ -97,6 +98,7 @@ function Book({ book }) {
                     axios.get(proxy + '/books/'+book._id)
                         .then(res => {
                             setLoading(false)
+                            dispatch(setLoading(false))
                             dispatch(setCurrent({...res.data, src: audio.src}))
                         })
                     
