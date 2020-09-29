918b844093d51c238722000b771c38a016fa3744
Дмитро Худіяш <khudiiash@MacBook-Pro-Dmitro.local>
2020-09-29 13:08:51 +0300

    init

--- server.js
+++ server.js
@@ -143,7 +143,10 @@ io.on('connection', function (socket) {
                                                 .then(function (stat) {
                                                     console.log('File Exists')
                                                     if (audio !== audioPath) {
-                                                        console.log("Good, we're there")
+                                                        console.log("Good, we're there, here's the torrent length", torrent.files.filter(f => /.mp3|\.aac|\.wav/.test(f.name)).length)
+                                                        console.log("title: ", findTitle(torrent.title))
+                                                        console.log("name: ", findTitle(torrent.name))
+
                                                         chapters = torrent.files.filter(f => /.mp3|\.aac|\.wav/.test(f.name)).length
                                                         console.log("Sending back", {title: findTitle(torrent.title), author: findAuthor(torrent.title), chapter: data.chapter, chapters, forFuture})
                                                         socket.emit('audio-loaded', {title: findTitle(torrent.title), author: findAuthor(torrent.title), chapter: data.chapter, chapters, forFuture})
